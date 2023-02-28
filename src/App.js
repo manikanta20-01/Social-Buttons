ecd989a3d5ecaa7da5e61bb1181c9b1c98f0fcf9
Namo manikanta <122387885+manikanta20-01@users.noreply.github.com>
2023-02-28 03:30:07 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="Heading"> Social Buttons </h1>
+        <div className="button">
+          <button className="Like"> Like </button>
+          <button className="comment"> Comment </button>
+          <button className="share"> Share </button>
+        </div>
+      </div>
+    </div>
+  );
+}
